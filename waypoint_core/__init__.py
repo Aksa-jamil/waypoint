11b67e5a5185e664302f53aca8a3e1c927ffcb90
Aksa Jamil <aksa.jamil786@gmail.com>
2026-08-19 21:54:44 -0400

    Set up Django project

---
+++ waypoint_core/__init__.py
@@ -0,0 +1,25 @@
+from waypoint.distance import Distance
+from waypoint.itinerary import Itinerary
+from waypoint.trail import (
+    Trail,
+    DayHike,
+    BackpackingRoute,
+    TrailRun,
+    GuidedDayHike,
+    ElevationMixin,
+    RatingMixin,
+    RatedDayHike,
+)
+
+__all__ = [
+    "Distance",
+    "Itinerary",
+    "Trail",
+    "DayHike",
+    "BackpackingRoute",
+    "TrailRun",
+    "GuidedDayHike",
+    "ElevationMixin",
+    "RatingMixin",
+    "RatedDayHike",
+]

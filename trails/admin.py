f03b91381909539a664abf258b7246c7050dc81b
Aksa Jamil <aksa.jamil786@gmail.com>
2026-08-20 15:41:20 -0400

    Add park relationships and trail filtering

--- trails/admin.py
+++ trails/admin.py
@@ -1,16 +1,26 @@
 from django.contrib import admin
 
-from .models import Trail
+from .models import Park, Trail
+
+
+@admin.register(Park)
+class ParkAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "region",
+    )
+    search_fields = ("name", "region")
 
 
 @admin.register(Trail)
 class TrailAdmin(admin.ModelAdmin):
     list_display = (
         "name",
+        "park",
         "distance_km",
         "elevation_gain",
         "difficulty",
         "is_open",
         "added",
     )
-    search_fields = ("name",)
+    search_fields = ("name", "park__name")
--- trails/models.py
+++ trails/models.py
@@ -9,6 +9,13 @@ class Trail(models.Model):
     ]
 
     name = models.CharField(max_length=200)
+    park = models.ForeignKey(
+        "Park",
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        related_name="trails",
+    )
     distance_km = models.DecimalField(max_digits=6, decimal_places=2)
     elevation_gain = models.IntegerField()
     difficulty = models.CharField(
@@ -25,6 +32,14 @@ class Trail(models.Model):
     @property
     def elevation(self):
         return self.elevation_gain
+
+    def __str__(self):
+        return self.name
+
+
+class Park(models.Model):
+    name = models.CharField(max_length=200)
+    region = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
